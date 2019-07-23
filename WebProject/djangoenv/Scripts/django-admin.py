d76cde72e4f8d454711868502997155d9b06e297
yuchanmo <mojjijji@gmail.com>
2019-07-24 00:18:34 +0900

    초기 커밋.

---
+++ WebProject/djangoenv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!D:\Programming\django\WebProject\WebProject\djangoenv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
---
+++ djangoroject/polls/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+# Create your views here.
+
+def index(request):
+    return HttpResponse('Hello world')
+
+def detail(request,question_id):
+    return HttpResponse('you`re looking questions %s'%(question_id))
+
+def results(request,question_id):
+    response = 'You`re looking at the results of question %s.'
+    return HttpResponse(response%(question_id))
+
+def vote(request,questions_id):
+    return HttpResponse("you`re voting on question %s"%(question_id))
